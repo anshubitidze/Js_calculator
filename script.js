f846e174db9862dbe0c55cdcdd0a5fb8a98ce54e
ShubitidzeA <ana.shubitidze@quantori.com>
2024-01-28 12:52:21 +0400

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+var userNum1 = window.prompt("Enter first number: ");
+var userNum2 = window.prompt("Enter second number: ");
+userNum1 = parseFloat(userNum1);
+userNum2 = parseFloat(userNum2)
+document.write(userNum1+userNum2)
+
+function myfunction(){
+    
+}
